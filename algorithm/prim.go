ab9fec9bbc86f61d4601566f65e7b3636a9804bf
Marius Neugebauer <mn@westwerk.ac>
2016-04-20 14:52:36 +0200

    simplified prim

--- algorithm/prim.go
+++ algorithm/prim.go
@@ -9,14 +9,6 @@ func (this Graph) PrimLength(start graphLib.VertexInterface) float64 {
     return PrimLength(this, start)
 }
 
-// helper struct for the prim algorithm
-type primVertex struct {
-    // if the vertex was visited
-    visited bool
-    // the item in the queue
-    item    *graphLib.NearestVertexQueueItem
-}
-
 // prim algorithm with result length
 func PrimLength(graph Graph, start graphLib.VertexInterface) float64 {
 
@@ -24,10 +16,7 @@ func PrimLength(graph Graph, start graphLib.VertexInterface) float64 {
     num := graph.GetVertices().Count()
 
     // map vertices to primVertex (holds the visited flag, the vertex of the new graph and the queue item)
-    vertices := make(map[graphLib.VertexInterface]*primVertex, num)
-
-    // initialize with the start vertex
-    vertices[start] = &primVertex{}
+    vertices := make(map[graphLib.VertexInterface]*graphLib.NearestVertexQueueItem, num)
 
     // create queue
     q := graphLib.NewNearestVertexQueue(num)
@@ -36,14 +25,13 @@ func PrimLength(graph Graph, start graphLib.VertexInterface) float64 {
     length := float64(0)
 
     // go through queue
-    for v, d := start, float64(0); v != nil; v, d, _ = q.PopNearestVertex() {
+    for v, d, n := start, float64(0), graphLib.VertexInterface(nil); v != nil; v, d, n = q.PopNearestVertex() {
 
         // add length
         length += d
 
         // vertex is visited
-        vv := vertices[v]
-        vv.visited = true
+        vertices[v] = nil
 
         // search all edges (ignore direction of the graph)
         for _, all := range v.GetEdgesFast() {
@@ -52,23 +40,25 @@ func PrimLength(graph Graph, start graphLib.VertexInterface) float64 {
                 // get the vertex that is not v
                 neighbourVertex := edge.GetOtherVertex(v)
 
+                // skip origin
+                if neighbourVertex == n {
+                    continue
+                }
+
                 // get the weight between v nd neighbourVertex
                 weight := edge.GetWeight()
 
                 // check if we know that vertex already
                 if neighbour, ok := vertices[neighbourVertex]; ok {
-                    if !neighbour.visited && neighbour.item.Weight > weight {
+                    if neighbour != nil && neighbour.Weight > weight {
                         // if it is not visited and we discovered a edge with lower weight
-                        neighbour.item.Weight = weight
-                        neighbour.item.From = v
-                        q.UpdatedVertex(neighbour.item)
+                        neighbour.Weight = weight
+                        neighbour.From = v
+                        q.UpdatedVertex(neighbour)
                     }
                 } else {
-                    // undiscovered vertex: add to map
-                    vertices[neighbourVertex] = &primVertex{
-                        // add to queue
-                        item: q.PushVertex(neighbourVertex, weight, v),
-                    }
+                    // undiscovered vertex: add to queue
+                    vertices[neighbourVertex] = q.PushVertex(neighbourVertex, weight, v)
                 }
             }
         }
