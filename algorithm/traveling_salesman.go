1b06971e8f1d831e8a3a03248c80660926990aa3
Marius Neugebauer <mn@westwerk.ac>
2016-05-09 03:50:07 +0200

    tsbf: cleaned up code

--- algorithm/traveling_salesman.go
+++ algorithm/traveling_salesman.go
@@ -5,6 +5,9 @@ import (
     "math"
 )
 
+// the maximum supported number of vertices in the traveling salesman brute force algorithm
+const tsbfMax = 15
+
 // simple wrapper
 func (this Graph) TravelingSalesmanBruteForce() float64 {
     return TravelingSalesmanBruteForce(this)
@@ -13,44 +16,68 @@ func (this Graph) TravelingSalesmanBruteForce() float64 {
 // returns the length of the shortest hamilton circle by brute force
 func TravelingSalesmanBruteForce(graph Graph) float64 {
 
+    // ----------------------------------------------
+    // check pre-requirements
+    // ----------------------------------------------
+
     // get the number of vertices
     num := int(graph.GetVertices().Count())
+
     // this algorithm only works with 5 to 15 vertices
-    if num <= 4 || num > 15 {
+    if num <= 4 || num > tsbfMax {
         return 0
     }
-    numMinus1 := num - 1
 
+    // ----------------------------------------------
+    // preparations
+    // ----------------------------------------------
+
+    // we access this one often, so better calculate it only once
+    lastVertex := num - 1
+
+    // use own vertex structure that has an index which refers to its position in the vertices array
+    // it also contains an array to each other position containing the distance between the vertices
     type vertex struct {
         graphLib.VertexInterface
         index     int
-        distances [15]float64
+        distances [tsbfMax]float64
     }
 
-    // create vertices
-    var vertices [15]*vertex
+    // create vertices and calculate distance once here instead of doing it multiple times later
+    var vertices [tsbfMax]*vertex
     for i, v := range graph.GetVertices().All() {
 
+        // create vertex
         vertices[i] = &vertex{
             VertexInterface: v,
             index: i,
         }
 
-        for i2, v2 := range vertices[:i] {
+        // add distances for all previously created vertices
+        for j, v2 := range vertices[:i] {
             w := graph.getWeightBetween(v, v2.VertexInterface)
-            vertices[i].distances[i2] = w
+            // make the distances two-way
+            vertices[i].distances[j] = w
             v2.distances[i] = w
         }
 
     }
 
-    // start
-    startDist := vertices[0].distances
-
-    // performs brute force to find the length of the shortest hamilton circle
-    type helperFunc func(int, *vertex, *vertex, float64)
-    var helpers [15]helperFunc
+    // the shortest circle's length will be stored in here
     length := math.MaxFloat64
+
+    // ----------------------------------------------
+    // build helpers
+    // ----------------------------------------------
+    // There will be 2 different helpers:
+    // * The intermediate helper will be called recursively and ...
+    //   ... creates all possible permutations of the remaining vertices.
+    // * The end helper will be called at the end of the recursion and ...
+    //   ... updates the length variable if a new shortest circle was found.
+    // ----------------------------------------------
+
+    // there will be 2 different helper functions. both will be stored in this array
+    var helpers [tsbfMax - 2]func(int, *vertex, *vertex, float64)
 
     // end helper
     helpers[num - 3] = func(n int, front, end *vertex, currentLength float64) {
@@ -61,7 +88,7 @@ func TravelingSalesmanBruteForce(graph Graph) float64 {
         }
     }
 
-    // front to pre-end helpers
+    // intermediate helper
     for i := num - 4; i >= 1; i-- {
         helpers[i] = func(n int, front, end *vertex, currentLength float64) {
 
@@ -72,7 +99,7 @@ func TravelingSalesmanBruteForce(graph Graph) float64 {
             next(n1, rest0, end, currentLength + frontDist[rest0.index])
 
             // combinations of changing the order
-            for i := n1; i < numMinus1; i++ {
+            for i := n1; i < lastVertex; i++ {
 
                 // change order
                 rest0Temp, vertices[i] = vertices[i], rest0
@@ -86,28 +113,54 @@ func TravelingSalesmanBruteForce(graph Graph) float64 {
         }
     }
 
-    currentLength, rest0, rest0Temp, end, endTemp, frontDist := 0.0, vertices[1], (*vertex)(nil), vertices[numMinus1], (*vertex)(nil), &vertices[0].distances
-    next := helpers[1]
+    // ----------------------------------------------
+    // Strategy
+    // ----------------------------------------------
+    // 1. Pick 0 as start vertex.
+    // 2. Pick every _set_ of two vertices and put one left and one right of the start vertex
+    // 3. Use every permutation of the remaining vertices to close the circle and calculate the length.
+    //    Use recursion for the permutation.
+    // ----------------------------------------------
 
-    // combinations of changing the order
-    for i := 1; i < numMinus1; i++ {
+    // ----------------------------------------------
+    // 1. Pick 0 as start vertex.
+    // ----------------------------------------------
+    startDist := vertices[0].distances
 
-        // change order
+    // ----------------------------------------------
+    // 2. Pick every _set_ of two vertices and put one left and one right of the start vertex
+    // ----------------------------------------------
+
+    // when swapping vertices, use this variables for the front and end to read from (faster than array access)
+    rest0, end := vertices[1], vertices[lastVertex]
+
+    // when swapping vertices, use this variables for the front and end to write to (faster than array access)
+    var rest0Temp, endTemp *vertex
+
+    // select first element of the set
+    for i := 1; i < lastVertex; i++ {
+
+        // swap. we do not actually need to swap, ...
+        // ... because on the next recursion levels only the indexes 2 upwards are accessed
         rest0Temp, vertices[i] = vertices[i], rest0
 
-        for j := i + 1; j < num; j++ {
+        // select second element of the set that is greater than the first one
+        for j := i + 1; j <= lastVertex; j++ {
 
-            // change order
+            // swap. we do not actually need to swap, ...
+            // ... because on the next recursion levels the last vertex is never accessed
             endTemp, vertices[j] = vertices[j], end
 
-            // recursion
-            next(2, rest0Temp, endTemp, currentLength + frontDist[rest0Temp.index] + startDist[endTemp.index])
+            // ----------------------------------------------
+            // 3. Use every permutation of the remaining vertices to close the circle and calculate the length.
+            // ----------------------------------------------
+            helpers[1](2, rest0Temp, endTemp, startDist[i] + startDist[j])
 
-            // change back
+            // swap back. since we did not really swap above, this is as simple as an assignment
             vertices[j] = endTemp
         }
 
-        // change back
+        // swap back. since we did not really swap above, this is as simple as an assignment
         vertices[i] = rest0Temp
     }
 
