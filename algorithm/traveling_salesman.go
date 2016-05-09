846273215ac31ca223af4335ad3d4be0d55f8630
Marius Neugebauer <mn@westwerk.ac>
2016-05-09 02:55:52 +0200

    tsbf: even more optimization

--- algorithm/traveling_salesman.go
+++ algorithm/traveling_salesman.go
@@ -48,14 +48,14 @@ func TravelingSalesmanBruteForce(graph Graph) float64 {
     startDist := vertices[0].distances
 
     // performs brute force to find the length of the shortest hamilton circle
-    type helperFunc func(int, *vertex, float64)
+    type helperFunc func(int, *vertex, *vertex, float64)
     var helpers [15]helperFunc
     length := math.MaxFloat64
 
     // end helper
-    helpers[num - 3] = func(n int, front *vertex, currentLength float64) {
-        rest0index, rest1 := vertices[n].index, vertices[n + 1]
-        l := currentLength + front.distances[rest0index] + rest1.distances[rest0index] + startDist[rest1.index]
+    helpers[num - 3] = func(n int, front, end *vertex, currentLength float64) {
+        rest0index := vertices[n].index
+        l := currentLength + front.distances[rest0index] + end.distances[rest0index]
         if l < length {
             length = l
         }
@@ -63,45 +63,52 @@ func TravelingSalesmanBruteForce(graph Graph) float64 {
 
     // front to pre-end helpers
     for i := num - 4; i >= 1; i-- {
-        helpers[i] = func(n int, front *vertex, currentLength float64) {
+        helpers[i] = func(n int, front, end *vertex, currentLength float64) {
 
-            n1, rest0, rest0b, frontDist := n + 1, vertices[n], (*vertex)(nil), &front.distances
+            n1, rest0, rest0Temp, frontDist := n + 1, vertices[n], (*vertex)(nil), &front.distances
             next := helpers[n]
 
             // when not changing the order
-            next(n1, rest0, currentLength + frontDist[rest0.index])
+            next(n1, rest0, end, currentLength + frontDist[rest0.index])
 
             // combinations of changing the order
             for i := n1; i < numMinus1; i++ {
 
                 // change order
-                rest0b, vertices[i] = vertices[i], rest0
+                rest0Temp, vertices[i] = vertices[i], rest0
 
                 // recursion
-                next(n1, rest0b, currentLength + frontDist[rest0b.index])
+                next(n1, rest0Temp, end, currentLength + frontDist[rest0Temp.index])
 
                 // change back
-                vertices[i] = rest0b
+                vertices[i] = rest0Temp
             }
         }
     }
 
-    currentLength, rest0, rest0b, frontDist := 0.0, vertices[1], (*vertex)(nil), &vertices[0].distances
+    currentLength, rest0, rest0Temp, end, endTemp, frontDist := 0.0, vertices[1], (*vertex)(nil), vertices[numMinus1], (*vertex)(nil), &vertices[0].distances
     next := helpers[1]
 
     // combinations of changing the order
     for i := 1; i < numMinus1; i++ {
+
+        // change order
+        rest0Temp, vertices[i] = vertices[i], rest0
+
         for j := i + 1; j < num; j++ {
 
             // change order
-            rest0b, vertices[i], vertices[numMinus1], vertices[j] = vertices[i], rest0, vertices[j], vertices[numMinus1]
+            endTemp, vertices[j] = vertices[j], end
 
             // recursion
-            next(2, rest0b, currentLength + frontDist[rest0b.index])
+            next(2, rest0Temp, endTemp, currentLength + frontDist[rest0Temp.index] + startDist[endTemp.index])
 
             // change back
-            vertices[i], vertices[numMinus1], vertices[j] = rest0b, vertices[j], vertices[numMinus1]
+            vertices[j] = endTemp
         }
+
+        // change back
+        vertices[i] = rest0Temp
     }
 
     // returns the length of the shortest hamilton circle
